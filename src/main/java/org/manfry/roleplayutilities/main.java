bcd1b6188236af0915b27650578d83327e94531c
Manfredi <59978676+Manfry0@users.noreply.github.com>
2022-07-05 22:27:56 +0200

    1.0

--- src/main/java/org/manfry/roleplayutilities/main.java
+++ src/main/java/org/manfry/roleplayutilities/main.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.manfry.roleplayutilities.commands.broadcast;
 import org.manfry.roleplayutilities.commands.help;
+import org.manfry.roleplayutilities.commands.roleplayutilities;
 
 public final class main extends JavaPlugin {
 
@@ -20,6 +21,7 @@ public final class main extends JavaPlugin {
 
         getCommand("broadcast").setExecutor(new broadcast());
         getCommand("help").setExecutor(new help());
+        getCommand("roleplayutilities").setExecutor(new roleplayutilities());
     }
 
     @Override
